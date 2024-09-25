9e1cf232fff7d7d36a86bdc671aa01b5cd8333b4
juliosshoji <zotokoba@gmail.com>
2024-09-24 23:43:33 -0300

    Arrumando referencias

--- pilhaDin.c
+++ pilhaDin.c
@@ -7,67 +7,67 @@ int Inicializar_pilha(node** pilha){
     return 0;
 };
 
-int Inicializar_pilha_vazia(node* pilha){
-    node* percorre = pilha;
+int Inicializar_pilha_vazia(node** pilha){
+    node* percorre = *pilha;
     while(percorre->next != NULL){
-        percorre = pilha->next;
-        free(pilha);
-        pilha = percorre;
+        percorre = percorre->next;
+        free(*pilha);
+        *pilha = percorre;
     }
-    free(pilha);
-    Inicializar_pilha(pilha);
+    free(*pilha);
+    Inicializar_pilha(*pilha);
     return 0;
 };
 
-int Inserir_topo(node* pilha, int info){
+int Inserir_topo(node** pilha, int info){
     node* aux = (node*)malloc(sizeof(node));
     if(aux == NULL){
         printf("Error alocating node\n");
         return 1;
     }
-    if(pilha == NULL){
+    if(*pilha == NULL){
         aux->next = NULL;
     } else {
-        aux->next = pilha;
+        aux->next = *pilha;
     }
     aux->info = info;
-    pilha = aux;
+    *pilha = aux;
     return 0;
 }
 
-int Remover_topo(node* pilha){
-    node* aux = pilha->next;
-    free(pilha);
-    pilha = aux;
+int Remover_topo(node** pilha){
+    node* aux = (*pilha)->next;
+    free(*pilha);
+    *pilha = aux;
     return 0;
 };
 
-int Obter_topo(node* pilha, int* info){
+int Obter_topo(node** pilha, int* info){
     if(Verifica_vazio(pilha))
         return 1;
-    *info = pilha->info;
+    *info = (*pilha)->info;
     return 0;
 };
 
-int Verifica_vazio(node* pilha){
-    if(pilha == NULL){
+int Verifica_vazio(node** pilha){
+    if(*pilha == NULL){
         return 1;
     }
     return 0;
 };
 
 //pilha1 vira topo
-int Juntar_pilhas(node* pilha1, node* pilha2){
-    node* percorre = pilha1;
+int Juntar_pilhas(node** pilha1, node** pilha2){
+    node* percorre = *pilha1;
     while(percorre->next != NULL){
         percorre = percorre->next;
     }
-    percorre->next = pilha2;
+    percorre->next = *pilha2;
     return 0;
 };
 
-int Obter_maior(node* pilha, int* maior){
-    node* percorre = pilha;
+int Obter_maior(node** pilha, int* maior){
+    node* percorre = *pilha;
     *maior = percorre->info;
     while(percorre->next != NULL){
         percorre = percorre->next;

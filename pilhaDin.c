67b1ab752d91a96e2a003be653d6b45ecc7e0ed1
juliosshoji <zotokoba@gmail.com>
2024-09-24 23:24:16 -0300

    Modularizando e corrigindo features

--- pilhaDin.c
+++ pilhaDin.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//Pilha dinamica
-typedef struct Tnode{
-    int dado;
-    struct Tnode* prox;
-} node;
-
-int Inicializar_pilha(node** pilha);
-int Incializar_pilha_vazia(node** pilha);
-int Inserir_topo(node** pilha, int dado);
-int Remover_topo(node** Pilha);
-int Obter_topo(node** pilha, int* info);
-int Verifica_vazio(node** pilha);
+#include "pilhaDin.h"
 
 int main(int argc, char* argv[]){
 
     node* P;
-    printf("%d\n", Inicializar_pilha(&P));
+    int info;
+    Inicializar_pilha(&P);
+    
+    Inserir_topo(&P, 10);
+    Inserir_topo(&P, 1);
+    Inserir_topo(&P, 0);
+    Inserir_topo(&P, 70);
+    Inserir_topo(&P, 6);
+    Inserir_topo(&P, 20);
+    Inserir_topo(&P, 7);
+    
     printf("%d\n", Verifica_vazio(&P));
-
+    Obter_topo(&P, &info);
+    printf("%d\n", info);
+    Obter_maior(&P, &info);
+    printf("%d\n", info);
+    
     return 0;
 };
 
@@ -30,8 +32,8 @@ int Inicializar_pilha(node** pilha){
 
 int Inicializar_pilha_vazia(node** pilha){
     node* aux1;
-    while((*pilha)->prox != NULL){
-        aux1 = (*pilha)->prox;
+    while((*pilha)->next != NULL){
+        aux1 = (*pilha)->next;
         free(*pilha);
         *pilha = aux1;
     }
@@ -40,20 +42,24 @@ int Inicializar_pilha_vazia(node** pilha){
     return 0;
 };
 
-int Inserir_topo(node** pilha, int dado){
+int Inserir_topo(node** pilha, int info){
     node* aux = (node*)malloc(sizeof(node));
     if(aux == NULL){
         printf("Error alocating node\n");
         return 1;
     }
-    aux->dado = dado;
-    aux->prox = *pilha;
+    if(*pilha == NULL){
+        aux->next = NULL;
+    } else {
+        aux->next = *pilha;
+    }
+    aux->info = info;
     *pilha = aux;
     return 0;
 }
 
 int Remover_topo(node** pilha){
-    node* aux = (*pilha)->prox;
+    node* aux = (*pilha)->next;
     free(*pilha);
     *pilha = aux;
     return 0;
@@ -62,13 +68,35 @@ int Remover_topo(node** pilha){
 int Obter_topo(node** pilha, int* info){
     if(Verifica_vazio(pilha))
         return 1;
-    *info = (*pilha)->dado;
+    *info = (*pilha)->info;
     return 0;
 };
 
 int Verifica_vazio(node** pilha){
     if(*pilha == NULL){
         return 1;
+    }
+    return 0;
+};
+
+//pilha1 vira topo
+int Juntar_pilhas(node** pilha1, node** pilha2){
+    node* percorre = *pilha1;
+    while(percorre->next != NULL){
+        percorre = percorre->next;
+    }
+    percorre->next = *pilha2;
+    return 0;
+};
+
+int Obter_maior(node** pilha, int* maior){
+    node* percorre = *pilha;
+    *maior = percorre->info;
+    while(percorre->next != NULL){
+        percorre = percorre->next;
+        if(percorre->info > *maior){
+            *maior = percorre->info;
+        }
     }
     return 0;
 };
---
+++ pilhaDin.h
@@ -0,0 +1,14 @@
+typedef struct Tnode{
+    int info;
+    struct Tnode* next;
+} node;
+
+int Inicializar_pilha(node** pilha);
+int Incializar_pilha_vazia(node** pilha);
+int Inserir_topo(node** pilha, int info);
+int Remover_topo(node** Pilha);
+int Obter_topo(node** pilha, int* info);
+int Verifica_vazio(node** pilha);
+
+int Juntar_pilhas(node** pilha1, node** pilha2);
+int Obter_maior(node** pilha, int* maior);

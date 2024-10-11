89d18ff781fbd16b64c7dfab0cfe5f12620a5979
juliosshoji <zotokoba@gmail.com>
2024-10-11 16:56:10 -0300

    Finalizando BubbleSort

--- BubbleSort/BubbleSort.c
+++ BubbleSort/BubbleSort.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #define INCREMENT 8
+#define M -100000
+#define N 100000
 
 typedef struct {
     int* vector;
@@ -9,7 +11,7 @@ typedef struct {
     int lastNumIndex;
 } vector;
 
-int push_back(vector* list);
+int push_back(vector* list, int* number);
 int pop(vector* list);
 int generateRandom(vector* list);
 int sort(vector* list);
@@ -30,8 +32,8 @@ int main(int argc, char* arv[]){
         printf("\n3) Insert New Number");
         printf("\n4) Remove Number");
         printf("\n5) Print list");
+        printf("\n6) Sort list");
         printf("\n\n");
-        printf("");
         scanf("%d", &option);
         
         switch(option){
@@ -42,13 +44,16 @@ int main(int argc, char* arv[]){
                 error = generateRandom(list);
                 break;
             case 3:
-                error = push_back(list);
+                error = push_back(list, (int*)NULL);
                 break;
             case 4:
                 error = pop(list);
                 break;
             case 5:
                 error = printVector(list);
+                break;
+            case 6:
+                error = sort(list);
                 break;
             default:
                 printf("Invalid input try! Try again!\n");
@@ -82,10 +87,12 @@ vector* createVector(){
     return list;
 }
 
-int push_back(vector* list){
-    int number;
-    printf("\nWhat number to insert?\n");
-    scanf("%d", &number);
+int push_back(vector* list, int* number){
+    
+    if(number == NULL){
+        printf("\nWhat number to insert?\n");
+        scanf("%d", (int*)number);
+    }
 
     if(list->size == list->lastNumIndex){
         list->vector = (int*)reallocarray(list->vector, list->size*2, sizeof(int));
@@ -96,21 +103,84 @@ int push_back(vector* list){
 
         list->size *= 2;
     }
-    list->vector[list->lastNumIndex+1] = number;
+    list->vector[list->lastNumIndex+1] = *((int*)number);
     list->lastNumIndex++;
 
     return 0;
 }
 
 int generateRandom(vector* list){
+
+    printf("How many number to generate?\n");
+    int qtd;
+    scanf("%d", &qtd);
+    int newNumber;
+    while(qtd != 0){
+        
+        newNumber = M + rand() / (RAND_MAX / (N - M + 1) + 1);
+        push_back(list, &newNumber);
+        
+        qtd--;
+    }
+
     return 0;
 }
 
 int pop(vector* list){
+    if(list->lastNumIndex == -1){
+        printf("Empty list\n");
+        return 0;
+    }
+    printf("Where to remove? (index)\n");
+    int index;
+    while(1){
+        scanf("%d", &index);
+        if(index > list->lastNumIndex){
+            printf("No number in list with index %d, try again: \n", index);
+        } else {
+            break;
+        }
+    }
+    int next;
+
+    for(int indexlist = index; indexlist <= list->lastNumIndex; indexlist++, index++){
+        if(list->lastNumIndex != 0 && indexlist != list->lastNumIndex){
+            next = list->vector[indexlist+1];
+            list->vector[indexlist] = next;
+        }
+    }
+    list->lastNumIndex--;
+
+    if(list->lastNumIndex < (list->size/2)){
+        list->vector = (int*)reallocarray(list->vector, list->size/2, sizeof(int));
+        if(list->vector == NULL){
+            printf("Error realocating memory\n");
+            return 2;
+        }
+
+        list->size /= 2;
+    }
+    
     return 0;
 }
 
 int sort(vector* list){
+    int swapped = 0;
+    int temp;
+    for(int macro = list->lastNumIndex-1; macro > 0; macro--){
+        swapped = 0;
+        for(int micro = 0; micro <= macro; micro++){
+            if(list->vector[micro] > list->vector[micro + 1]){
+                temp = list->vector[micro];
+                list->vector[micro] = list->vector[micro + 1];
+                list->vector[micro + 1] = temp;
+                swapped = 1;
+            } 
+        }
+        if(swapped == 0){
+                break;
+        }
+    }
     return 0;
 }
 
